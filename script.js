db712fb99f2a55e18eab463b94f743695842e04d
Levy <102899@glr.nl>
2024-11-07 09:54:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let spaardoel;
+let prijs_spaardoel;
+let uurloon;
+let gewerkte_uren;
+let spaarpercentage;
+
+function berekenen() {
+    let spaardoel = document.getElementById("spaardoel").value;
+    let prijs_spaardoel = document.getElementById("prijs_spaardoel").value;
+    let uurloon = document.getElementById("uurloon").value;
+    let gewerkte_uren = document.getElementById("gewerkte_uren").value;
+    let spaarpercentage = document.getElementById("spaarpercentage").value;
+    console.log(spaardoel, prijs_spaardoel, uurloon, gewerkte_uren, spaarpercentage);
+}
+
